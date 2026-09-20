43b8487162ca50daf8fe2e0bfeb43e9ee60cf9f5
HenvagDev <henvagrolland@gmail.com>
2026-09-20 21:43:38 +0200

    Swap landing Google/Microsoft mark for wordmark asset.

--- frontend/src/Landing.tsx
+++ frontend/src/Landing.tsx
@@ -132,7 +132,7 @@ export default function Landing({
         <ul className="feature-list">
           <li>
             <span className="feature-icon feature-icon-wide">
-              <img src={idpPairMark} alt="" width={40} height={21} />
+              <img src={idpPairMark} alt="" height={18} />
             </span>
             <div>
               <strong>{t("feature.google.title")}</strong>
